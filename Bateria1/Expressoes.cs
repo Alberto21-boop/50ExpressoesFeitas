08a754a3969f65c969e563fdf597f1cf023d9cc0
alberto <albertobarbosa0003@gmail.com>
2022-05-15 14:47:11 -0300

    Adicionar arquivos de projeto.

---
+++ Bateria1/Expressoes.cs
@@ -0,0 +1,211 @@
+﻿namespace Bateria1
+{
+    public class Expressoes
+    {
+        public static void Main(string[] args)
+        {
+            //1) 7 + 8 x 9
+            int a = 7 + (8 * 9);
+            Console.WriteLine(a);
+
+            //2) 8 + 7 x 9
+            int b = 8 + (7 * 9);
+            Console.WriteLine(b);
+
+            //3) 8 x 7 + 9
+            int c = (8 * 7) + 9;
+            Console.WriteLine(c);
+
+            //4) 9 + 72 : 8
+            double d = 9 + (72 / 8);
+            Console.WriteLine(d);
+
+            //5) 5 + 3 x 9=
+            int e = 5 + (3 * 9);
+            Console.WriteLine(e);
+
+            //6) 5 x 3 + 9=
+            int f = (5 * 3) + 9;
+            Console.WriteLine(f);
+
+            //7) 6 + 24 :8=
+            int g = 6 + (24 / 8);
+            Console.WriteLine(g);
+
+            //8) 24 : 6 + 8=
+            int h = (24 / 6) + 8;
+            Console.WriteLine(h);
+
+            //9) 17 – 40: 4=
+            int i = 17 - (40 / 4);
+            Console.WriteLine(i);
+
+            //10) 17 x 4 – 40=
+            int j = (17 * 4) - 40;
+            Console.WriteLine(j);
+
+            //11) 38 – 9 x 4=
+            int l = 38 - (9 * 4);
+            Console.WriteLine(l);
+
+            //12) 44 – 6 x 6=
+            int m = 44 - (6 * 6);
+            Console.WriteLine(m);
+
+            //13) 7 x 7 + 5=
+            int n = (7 * 7) + 5;
+            Console.WriteLine(n);
+
+            //14) 6 + 4 x 7 – 5
+            int o = 6 + (4 * 7) - 5;
+            Console.WriteLine(o);
+
+            //15) 8 + 4 x 6 – 6
+            int p = 8 + (4 * 6) - 6;
+            Console.WriteLine(p);
+
+            //16) 15 – 20: 4+ 8=
+            double q = 15 - (20 / 4) + 8;
+            Console.WriteLine(q);
+
+            //17) 49 – 45 :5 +4=
+            double r = 49 - (45 / 5) + 4;
+            Console.WriteLine(r);
+
+            //18) 21 – 64:8 + 2=
+            double s = 21 - (64 / 8) + 2;
+            Console.WriteLine(s);
+
+            //19) 3 + 7 x 9 – 13=
+            int t = 3 + (7 * 9) - 5;
+            Console.WriteLine(t);
+
+            //20) 30 – 65 : 5 +1=
+            double u = 30 - (65 / 5) + 1;
+            Console.WriteLine(u);
+
+            //21) 6 x 8 – 32 : 4=
+            double v = (6 * 8) - (32 / 4);
+            Console.WriteLine(v);
+
+            //22) 15 : 3 + 3 x 9 =
+            double w = (15 / 3) + (3 * 9);
+            Console.WriteLine(w);
+
+            //23) 6 x 3 +5 – 7 x 3=
+            double x = (6 * 3) + 5 - (7 * 3);
+            Console.WriteLine(x);
+
+            //24) 42:7 + 18 – 6 x 4=
+            double y = (42 / 7) + 18 - (6 * 4);
+            Console.WriteLine(y);
+
+            //25) 7x2 + 10 – 50 : 10=
+            double z = (7 * 2) + 10 - (50 / 10);
+            Console.WriteLine(z);
+
+            //26) 6 + 3 x 3 – 2 x 5 =
+            int a1 = 6 + (3 * 3) - (2 * 5);
+            Console.WriteLine(a1);
+
+            //27) 18 + 6 x 9 – 8 x 6=
+            int a2 = 12 + (6 * 9) - (8 + 6);
+            Console.WriteLine(a2);
+
+            //28) 15 + 3 – 8 x 2 +4=
+            int a3 = 15 + 3 - (8 * 2) + 4;
+            Console.WriteLine(a3);
+
+            //29) 7 x 2 x 5 – 50
+            int a4 = ((7 * 2) * 5) - 50;
+            Console.WriteLine(a4);
+
+            //30) 128 : 2 : 2 : 8 =
+            double a5 = 128 / 2 / 2 / 8;
+            Console.WriteLine(a5);
+
+            //31) 6 x (2 + 7)=
+            int a6 = 6 * (2 + 7);
+            Console.WriteLine(a6);
+
+            //32) 7 x (3 + 5)=
+            int b1 = 7 * (3 + 5);
+            Console.WriteLine(b1);
+
+            //33) 8 x ( 10 – 2)=
+            int b2 = 8 * (10 - 2);
+            Console.WriteLine(b2);
+
+            //34) (6 + 4 ) x 2=
+            int b3 = (6 + 4) * 2;
+            Console.WriteLine(b3);
+
+            //35) (6 – 4 ) x 9=
+            int b4 = (6 - 4) * 9;
+            Console.WriteLine(b4);
+
+            //36) 3 x ( 15 – 5)=
+            int b5 = 3 * (15 - 5);
+            Console.WriteLine(b5);
+
+            //37) (9 – 2 ) x 4 =
+            int c1 = (9 - 2) * 4;
+            Console.WriteLine(c1);
+
+            //38) 24 : ( 15 – 7)=
+            double c2 = 24 / (15 - 7);
+            Console.WriteLine(c2);
+
+            //39) 40 : ( 8 -3 )=
+            double c3 = 40 / (8 - 3);
+            Console.WriteLine(c3);
+
+            //40) 45 : ( 12 – 9)=
+            double c4 = 45 / (12 - 9);
+            Console.WriteLine(c4);
+
+            //41) (3 + 6 x 5 ) – 20=
+            int d1 = (3 + (6 * 5)) - 20;
+            Console.WriteLine(d1);
+
+            //42) ( 4+ 8 x 7) : 6=
+            double d2 = (4 + (8 * 7)) / 6;
+            Console.WriteLine(d2);
+
+            //43) (25 – 4 x 4) : 3=
+            double d3 = (25 - (4 * 4)) / 3;
+            Console.WriteLine(d3);
+
+            //44) 15 + (32 : 4-3 )=
+            double d4 = 15 + (32 / (4 - 3));
+            Console.WriteLine(d4);
+
+            //45) 35 : (4 x 3 -5)=
+            double d5 = 35 / ((4 * 3) - 5);
+            Console.WriteLine(d5);
+
+            //46) 7 x (15 : 3 + 5) =
+            double e1 = 7 * ((15 / 3) + 5);
+            Console.WriteLine(e1);
+
+            //47) 100– (6 x 8+ 2)=
+            int e2 = 100 - ((6 * 8) + 2);
+            Console.WriteLine(e2);
+
+            //48) ( 7 x 9 – 3) : 5=
+            double e3 = ((7 * 9) - 3) / 5;
+            Console.WriteLine(e3);
+
+            //49) ( 8 + 8 x 9 ) : 8 =
+            double e4 = (8 + (8 * 9)) / 8;
+            Console.WriteLine(e4);
+
+            //50) ( 6 + 6 x 6 ) : 6=
+            double e5 = (6 + (6 * 6)) / 6;
+            Console.WriteLine(e5);
+
+
+
+        }
+    }
+}
